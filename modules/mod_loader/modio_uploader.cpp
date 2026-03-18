16e18ffb695cc21bc638397d4d8aeede778784b6
nsapples <ideasewer.studios@gmail.com>
2026-03-17 17:16:13 -0700

    Fix multipart upload to send file first then text fields

--- modules/mod_loader/modio_uploader.cpp
+++ modules/mod_loader/modio_uploader.cpp
@@ -270,24 +270,30 @@ void ModIOUploader::upload_mod(const String &p_mod_path, const String &p_name, c
 		mod_version = ver_cfg->get_value("mod", "version", "1.0.0");
 	}
 
+	// Build multipart body - use simple boundary without extra dashes.
+	String b = boundary;
+
 	PackedByteArray body;
-	auto add_text_field = [&](const String &p_fname, const String &p_value) {
-		String part = vformat("--%s\r\nContent-Disposition: form-data; name=\"%s\"\r\n\r\n%s\r\n", boundary, p_fname, p_value);
-		body.append_array(part.to_utf8_buffer());
-	};
 
-	add_text_field("version", mod_version);
-	add_text_field("changelog", "Uploaded from Blossom Engine");
-	add_text_field("active", "true");
-
-	// Add the zip file.
-	String part_header = vformat("--%s\r\nContent-Disposition: form-data; name=\"filedata\"; filename=\"%s\"\r\nContent-Type: application/zip\r\n\r\n", boundary, file_name);
-	body.append_array(part_header.to_utf8_buffer());
+	// File field first.
+	String file_header = "--" + b + "\r\nContent-Disposition: form-data; name=\"filedata\"; filename=\"" + file_name + "\"\r\nContent-Type: application/zip\r\n\r\n";
+	body.append_array(file_header.to_utf8_buffer());
 	body.append_array(file_data);
 	body.append_array(String("\r\n").to_utf8_buffer());
 
-	String part_footer = vformat("--%s--\r\n", boundary);
-	body.append_array(part_footer.to_utf8_buffer());
+	// Text fields.
+	auto add_field = [&](const String &p_name, const String &p_value) {
+		String field = "--" + b + "\r\nContent-Disposition: form-data; name=\"" + p_name + "\"\r\n\r\n" + p_value + "\r\n";
+		body.append_array(field.to_utf8_buffer());
+	};
+
+	add_field("version", mod_version);
+	add_field("changelog", "Uploaded from Blossom Engine");
+	add_field("active", "true");
+
+	// End boundary.
+	String footer = "--" + b + "--\r\n";
+	body.append_array(footer.to_utf8_buffer());
 
 	String url = vformat("%s/games/%d/mods/%d/files", api_base_url, BLOSSOM_GAME_ID, pending_mod_id);
 	PackedStringArray headers;
